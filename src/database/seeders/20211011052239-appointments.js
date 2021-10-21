0183f233ea72106111fc26e5ee6a6bf6b0c9c375
LeoDLeao <leo.riegler1@gmail.com>
2021-10-21 10:19:35 -0300

    CRUD Implementation

--- src/database/seeders/20211011052239-appointments.js
+++ src/database/seeders/20211011052239-appointments.js
@@ -1,28 +1,52 @@
-'use strict';
+"use strict";
 
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		return queryInterface.bulkInsert(
-			'Appointments',
-			[
-				{
-					physicianId: 14,
-					patientId: 25,
-					appointmentDate: '2021-05-10',
-					description: 'Exam Analysis',
-				},
-				{
-					physicianId: 14,
-					patientId: 26,
-					appointmentDate: '2021-05-18',
-					description: 'Exam Analysis',
-				},
-			],
-			{}
-		);
-	},
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      "Appointments",
+      [
+        {
+          physicianId: 1,
+          patientId: 5,
+          appointmentDate: "2021-05-10",
+          description: "Exam Analysis",
+        },
+        {
+          physicianId: 1,
+          patientId: 6,
+          appointmentDate: "2021-05-18",
+          description: "Exam Analysis",
+        },
+        {
+          physicianId: 2,
+          patientId: 2,
+          appointmentDate: "2021-05-20",
+          description: "Exam Analysis",
+        },
+        {
+          physicianId: 2,
+          patientId: 1,
+          appointmentDate: "2021-05-21",
+          description: "Exam Analysis",
+        },
+        {
+          physicianId: 3,
+          patientId: 3,
+          appointmentDate: "2021-05-25",
+          description: "Exam Analysis",
+        },
+        {
+          physicianId: 3,
+          patientId: 6,
+          appointmentDate: "2021-05-26",
+          description: "Exam Analysis",
+        },
+      ],
+      {}
+    );
+  },
 
-	down: async (queryInterface, Sequelize) => {
-		return queryInterface.bulkDelete('Appointments', null, {});
-	},
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("Appointments", null, {});
+  },
 };
